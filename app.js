0981d219cd78e80fa0bad49465d20145f6a562ce
Thomas Poupinet <th.poupinet@free.fr>
2021-07-14 13:57:45 +0200

    forgut importssss

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use((req, res, next) => {
   });
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/auth', userRoutes);
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
